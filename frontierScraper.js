e348bc678014822b88d49d5505e3913f6cc218c7
andrewkmin <andrewmin8@gmail.com>
2017-04-20 00:11:38 -0700

    refactor

--- frontierScraper.js
+++ frontierScraper.js
@@ -1,81 +1,50 @@
-// NOTE: TODO -- take in two locations (from and to)
-
 function scraper(from, to){
-	var seaArr = [];
-	var phlArr = [];
-	var laxArr = [];
-	var sfoArr = [];
-	var snaArr = [];
-	var phxArr = [];
-	var nyArr = [];
+	var from = from.toUpperCase();
+	var to = to.toUpperCase();
+	var firstLegs = [];
+	var secondLegs = [];
 	var trips = document.getElementsByName('saleBox')
 	for(var i = 0; i < trips.length; i++){
 		var newObj = {};
-		if(trips[i].outerText.toUpperCase().includes('SEATTLE')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			seaArr.push(newObj)
-		} else if(trips[i].outerText.toUpperCase().includes('PHILADELPHIA')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			phlArr.push(newObj);
-		} else if(trips[i].outerText.toUpperCase().includes('LOS ANGELES')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			laxArr.push(newObj);
-		} else if(trips[i].outerText.toUpperCase().includes('SAN FRANCISCO')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			sfoArr.push(newObj);
-		} else if(trips[i].outerText.toUpperCase().includes('SANTA ANA')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			snaArr.push(newObj);
-		} else if(trips[i].outerText.toUpperCase().includes('PHOENIX')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			phxArr.push(newObj);
-		} else if(trips[i].outerText.toUpperCase().includes('NEW YORK')){
-			newObj.from = trips[i].firstChild.firstChild.innerText;
-			newObj.to = trips[i].firstChild.lastChild.innerText;
-			// newObj.price = trips[i].lastChild.children[1].children[0].getElementsByClassName('priceNonDiscountDen')[0].outerText;
-			newObj.price = trips[i].lastChild.children[1].children[0].innerText
-			newObj.date = trips[i].lastChild.children[1].children[1].innerText;
-			nyArr.push(newObj);
+		if(trips[i].firstChild.firstChild.innerText.toUpperCase().includes(from.toUpperCase())){
+			var firstTo = trips[i].firstChild.lastChild.innerText;
+			firstTo = firstTo.substring(4, firstTo.length);
+			firstTo = firstTo.substring(0, firstTo.indexOf(' ')).toUpperCase();
+			newObj.firstFrom = from;
+			newObj.firstTo = firstTo;
+			newObj.firstPrice = trips[i].lastChild.children[1].children[0].innerText
+			newObj.firstDate = trips[i].lastChild.children[1].children[1].innerText;
+			firstLegs.push(newObj);
+		}
+		newObj = {};
+		if(trips[i].firstChild.lastChild.innerText.toUpperCase().includes(to.toUpperCase())){
+			var secondFrom = trips[i].firstChild.firstChild.innerText;
+			secondFrom = secondFrom.substring(6, secondFrom.length);
+			secondFrom = secondFrom.substring(0, secondFrom.indexOf(' ')).toUpperCase();
+			newObj.secondFrom = secondFrom;
+			newObj.secondTo = to;
+			newObj.secondPrice = trips[i].lastChild.children[1].children[0].innerText
+			newObj.secondDate = trips[i].lastChild.children[1].children[1].innerText;
+			secondLegs.push(newObj);
+		}
+		newObj = {};
+	}
+	console.log('First legs', firstLegs);
+	console.log('Second legs', secondLegs);
+	var anotherArr = [];
+	for(var x = 0; x < firstLegs.length; x++){
+		for(var y = 0; y < secondLegs.length; y++){
+			if(firstLegs[x].firstTo === secondLegs[y].secondFrom){
+				newObj.from = from;
+				newObj.middle = firstLegs[x].firstTo;
+				newObj.to = to;
+				newObj.firstDate = firstLegs[x].firstDate;
+				newObj.secondDate = secondLegs[y].secondDate;
+				newObj.price = firstLegs[x].firstPrice.concat(secondLegs[y].secondPrice);
+				anotherArr.push(newObj);
+				newObj = {};
+			}
 		}
 	}
-	var finalObj = {
-		SEATTLE: seaArr,
-		PHILADELPHIA: phlArr,
-		LOSANGELES: laxArr,
-		SANFRANCISCO: sfoArr,
-		SANTAANA: snaArr,
-		PHOENIX: phxArr,
-		NEWYORK: nyArr
-	};
-
-	if(!location){
-		return finalObj;
-	} else {
-		return finalObj[location.toUpperCase().replace(/\s+/g, '')];
-	}
-	console.log('Finished.', finalObj);
+	console.log('Itinerary', anotherArr);
 }
